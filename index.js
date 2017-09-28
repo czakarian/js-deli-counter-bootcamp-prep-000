3d93eef984c2432fdc5f7f403398b0eab0379a7c
Christina Zakarian <christinazakarian@gmail.com>
2017-09-28 02:52:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing() {
 function currentLine() {
   if(katzDeliLine.length === 0)
   {
-    return "The line is currently empty"
+    return "The line is currently empty."
   }
   else {
     var output = "The line is currently: "
