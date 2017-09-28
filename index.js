5aa975f0709c5f1e5491a1a6c969b339eefaa9a5
Christina Zakarian <christinazakarian@gmail.com>
2017-09-28 03:12:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(katzDeliLine) {
   }
   else {
     var first = katzDeliLine[0]
-    katzDeliLine.unshift()
+    katzDeliLine.shift()
     return `Currently serving ${first}.`
   }
 }
